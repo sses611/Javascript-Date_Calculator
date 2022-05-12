a24651ec085a0f69785954881f7fb34606d65567
dans <sses611@gmail.com>
2022-05-13 00:46:03 +0900

    update

--- js/basic.js
+++ js/basic.js
@@ -2,42 +2,29 @@ $(document).ready(function () {
   const today = new Date();
   let datePick = "";
 
-  let now = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  let now =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
   $("#today").eq(0).text(now);
 
-  
-  $("#datepicker22").change(function () {
+  $("#datepicker2").change(function () {
     datePick = new Date($("#datepicker2").val());
+    alert(datePick);
 
     $("button").click(function () {
       let cal = datePick - today;
 
-      // while (true) {
-      //   if (cal != 0 && cal < 0) {
-      //     alert("이미 지난 날짜로 디데이 계산을 할 수 없습니다.");
-      //     break;
-      //   } if(cal > 0) {
-      //     cal = Math.abs(cal);
-      //     cal = parseInt(cal / 1000 / 60 / 60 / 24) + 1;
+      cal = parseInt(cal / 1000 / 60 / 60 / 24);
+      // cal = Math.abs(cal); // 양수로 변환
 
-      //     $(".dDay").eq(0).text(cal);
-      //     break;
-      //   }
-        cal = parseInt(cal / 1000 / 60 / 60 / 24);
-        // cal = Math.abs(cal); // 양수로 변환
-       
-       
-      if ( cal > 0) {
+      if (cal > 0) {
         cal = cal + 1;
-         $(".dDay").eq(0).text(cal);
-      }else if(cal < 0){
+        $(".dDay").eq(0).text(cal);
+      } else if (cal < 0) {
         alert(cal);
         $(".dDay").eq(0).text(cal);
-      }else{
+      } else {
         $(".dDay").eq(0).text(0);
       }
-
-      
     });
   });
 });
