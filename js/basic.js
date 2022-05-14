01fa985a49427935be1c360fae9d829d89562f63
dans <sses611@gmail.com>
2022-05-15 00:08:54 +0900

    d-day css update

--- js/basic.js
+++ js/basic.js
@@ -1,30 +1,27 @@
-$(document).ready(function () {
-  const today = new Date();
-  let datePick = "";
+  $(document).ready(function () {    
+    const today = new Date();
+    let datePick = "";
 
-  let now =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-  $("#today").eq(0).text(now);
+    let now =
+      today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+    $("#today").eq(0).text(now);
 
-  $("#datepicker2").change(function () {
-    datePick = new Date($("#datepicker2").val());
-    // alert(datePick);
+    $("#datepicker2").change(function () {
+      datePick = new Date($("#datepicker2").val());
+      // alert(datePick);
 
-    $("button").click(function () {
-      let cal = datePick - today;
+        let cal = datePick - today;
 
-      cal = parseInt(cal / 1000 / 60 / 60 / 24);
-      // cal = Math.abs(cal); // 양수로 변환
+        cal = parseInt(cal / 1000 / 60 / 60 / 24);
+        // cal = Math.abs(cal); // 양수로 변환
 
-      if (cal > 0) {
-        cal = cal + 1;
-        $(".dDay").eq(0).text(cal+"일");
-      } else if (cal < 0) {
-        alert(cal);
-        $(".dDay").eq(0).text(cal+"일");
-      } else {
-        $(".dDay").eq(0).text(0+"일");
-      }
+        if (cal > 0) {
+          cal = cal + 1;
+          $(".dDay").eq(0).text('D - ' + cal);
+        } else if (cal < 0) {
+          $(".dDay").eq(0).text('D + ' + (-1 * cal));
+        } else {
+          $(".dDay").eq(0).text(0);
+        }
+      });
     });
-  });
-});
